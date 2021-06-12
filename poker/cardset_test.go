61c29af5fee97a1120c02d96f6754547594c44d2
Adriano Hernandez <adrianoh@mit.edul>
2021-06-11 18:29:22 -0700

    hmm

--- poker/cardset_test.go
+++ poker/cardset_test.go
@@ -1,9 +1,9 @@
-package testing
+package poker
 
 import "testing"
 import "fmt"
 
-import "github.com/4gatepylon/GoPoker/poker/cardset"
+import "github.com/4gatepylon/GoPoker/poker"
 
 // Golang is smart and every file that is Test<string that doesn't start with a lowercase letter>
 // will be run when you do "go test" and we can use fmt.Errof to throw errors
@@ -11,17 +11,18 @@ import "github.com/4gatepylon/GoPoker/poker/cardset"
 // I might upgrade to Bazel later (though I can write my own rule in that case....)
 
 func TestRoyalFlushExist(t *testing.T) {
-
+	a := poker.T(true, 1, 0)
+	fmt.Println(a)
 }
 
 func TestRoyalFlushDoesNotExist(t *testing.T) {
-
+	fmt.Errorf("cunt")
 }
 
 func TestQuadsExist(t *testing.T) {
-
+	fmt.Errorf("cunt")
 }
 
 func TestQuadsDoNotExist(t *testing.T) {
-
+	fmt.Errorf("cunt")
 }
